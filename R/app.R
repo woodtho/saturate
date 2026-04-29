88fe8fa4cf46904e316ebd8e6a896a0c4cba0ba2
Thomas Wood <thomaswood1994@gmail.com>
2026-04-29 10:55:09 -0400

    Fix navbar alignment and increase login logo to 200px
    - navbar-brand: remove top/bottom padding, add flex centering so logo
      sits flush with nav links rather than floating off-axis
    - Increase navbar logo to 48px and add 2.5rem right margin
    - Login screen logo bumped to 200px

--- R/app.R
+++ R/app.R
@@ -144,7 +144,7 @@ saturate_ui <- function(app_name = "saturate", brand_css = "") {
         class = "qc-profile-brand",
         shiny::tags$img(
           src   = "saturate-assets/logo.png",
-          height = "160",
+          height = "200",
           alt   = "saturate",
           style = "display:block;margin:0 auto 1rem;"
         )
